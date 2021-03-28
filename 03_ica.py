c7f8687e28d409b5caade3c87c3e78497cdf2370
Stefan Ott <kicker17@gmx.de>
2021-03-28 21:54:35 +0200

    Fix minor ica issues and push missing epoc function

--- 03_ica.py
+++ 03_ica.py
@@ -8,6 +8,7 @@ subject = handleSubjectArg()
 
 # Load raw data of previous step
 raw = readRawFif(fname.cleaned(subject=subject), preload=True)
+raw_cleaned = raw.copy() #keep copy for overlay plot
 
 if config["isPrecomputeMode"]:
     ica, bad_components = load_precomputed_ica("local/bids", subject)
@@ -19,14 +20,16 @@ else:
     #ica specific high-pass filter with ~1hz to remove slow drifts
     # according to: https://mne.tools/dev/auto_tutorials/preprocessing/plot_40_artifact_correction_ica.html
     # and https://mne.tools/stable/generated/mne.preprocessing.ICA.html 
+    
     raw_filt = raw.copy()
     raw_filt.load_data().filter(l_freq=config["freq_highpass_ica"], h_freq=None)
 
     ica.fit(raw_filt, verbose=True)
 
-    #Apply ICA to original raw data after components are found
+    #Apply ICA to original raw data after components are found only if bad channels specified in config
+    assert subject in config["subject_ica_channels"]
     bad_components = config["subject_ica_channels"][subject]
-    raw_ica = ica.apply(raw, exclude=list(bad_components))
+    raw_ica = ica.apply(raw, exclude=np.array(bad_components).astype(int))
 
     #manually look at components and identify independent components
     # according to: https://labeling.ucsd.edu/tutorial/labels
@@ -51,5 +54,5 @@ if config["isSpaceSaveMode"]:
     os.remove(fname.cleaned(subject=subject))
 
 #VISUAL CHECK: plot ICA for overlay of cleaned signal
-fig_overlay = ica.plot_overlay(raw,exclude=np.array(bad_components).astype(int), show=False)
+fig_overlay = ica.plot_overlay(raw_cleaned,exclude=np.array(bad_components).astype(int), show=False)
 addFigure(subject, fig_overlay, "Overlay original/reconstructed:", "Preprocess")
