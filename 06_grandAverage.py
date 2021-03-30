f070ac8969cc68d57d7680fccd16dad2badae1a2
Stefan Ott <kicker17@gmx.de>
2021-03-30 12:30:23 +0200

    grand average peak plot added

--- 06_grandAverage.py
+++ 06_grandAverage.py
@@ -4,6 +4,7 @@ import ccs_eeg_utils
 from config import config, fname, n_jobs
 from mne_bids import (BIDSPath, read_raw_bids)
 from matplotlib import pyplot as plt
+import matplotlib
 from utils import handleSubjectArg, readRawFif, addFigure
 import json, os
 import numpy as np
@@ -42,16 +43,25 @@ average = {config["event_names"]["cond1"]: cond1_average, config["event_names"][
 fig_evokeds = mne.viz.plot_compare_evokeds(average, picks=config["pick"], show=True if config["isDialogeMode"] else False)
 
 
+#data line fitted
+fig_hist, ax_hist = plt.subplots()
+ax_hist.title.set_text("Peak data fitted curve")
+ax_hist.set_xlabel("Number of peaks")
+ax_hist.set_ylabel("Peak Amplitude [V]")
+(mean, sigma) = stats.norm.fit(np.array(peak_list))
+n, bins, patches = ax_hist.hist(np.array(peak_list), 20,  alpha=0.15, orientation="horizontal")
+y = stats.norm.pdf(bins, mean, sigma)
+normed = y*(np.amax(n)/np.amax(y))
+l = ax_hist.plot(normed, bins, 'royalblue', linewidth=2)
+
+
 #RQ: On which ERP-peaks do we find major difference between the conditions
 #statistically test with t-test
-fig_hist, ax = plt.subplots()
-ax.hist(peak_list, bins=15)
-
 t_values, p_value = stats.ttest_1samp(peak_list, 0.0, alternative="greater")
 
 
 result_str = "T-test: p-value {} {} {} alpha".format(p_value, ">=" if p_value > config["alpha"] else "<", config["alpha"])
 print(result_str)
-print("Statistically it is {} that the {} ERP is present given this data".format("significant" if p_value < config["alpha"] else "unsignificant", config["task"]))
+print("Statistically it is {} that the {} ERP is present given this data at {}".format("significant" if p_value < config["alpha"] else "unsignificant", config["task"], config["pick"]))
 addFigure(None, fig_evokeds, "Evoked Plot ","Peak-Analysis", totalReport=True, comments=result_str)
 addFigure(None, fig_hist, "Peak histogram","Peak-Analysis", totalReport=True)
