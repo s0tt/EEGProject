ca71b8f9b7570b121001997450023e1ab3e74179
Stefan Ott <kicker17@gmx.de>
2021-03-30 16:24:22 +0200

    Improve decoding

--- 09_decoding_01.py
+++ 09_decoding_01.py
@@ -35,80 +35,68 @@ def getEpochLabels(epochs):
     labels = [int(label == "cond2") for label in labels]
     return np.array(labels)
 
-def plotData(ax, x, y, model_type, t_max):
+def plotData(x, y, model_type, t_max):
+    fig, ax = plt.subplots(constrained_layout=True, figsize=(16, 9))
     ax.plot(x, y, label="score")
     ax.set_xlabel("Time (s)")
     ax.set_ylabel("Score: "+config["decode_scoring"])
-    ax.set_xlim([0.0, 0.8])
-    ax.set_ylim([0.2, 1.0])
-    ax.set_title(model_type +" decoding scores")
+    ax.set_xlim([0, 1])
+    ax.set_ylim([0, 1.0])
+    ax.set_title(model_type +" scores")
     ax.legend()
-    ax.grid(linestyle="dashed")
+    ax.grid(alpha=0.25)
     ax.set_axisbelow(True)
     ax.xaxis.set_major_locator(ticker.MultipleLocator(0.1))
-    ax.axhline(0.5, label="random")
-    ax.axvline(x=t_max, label="peak")
+    ax.axhline(0.5, color="r", linestyle='--', label="random")
+    #ax.axvline(x=t_max, color="r", label="peak")
+    return fig
 
 
-if not os.path.exists(fname.decodingAnalysis(subject=subject)) and not config["isDialogeMode"]:
-    epochs = mne.read_epochs(fname.epochs(subject=subject))
-    epochs.resample(256)
-    epochs = epochs[["cond1", "cond2"]]
-    epochs = epochs.crop(tmin=-0.1, tmax=1.0)
-    labels = getEpochLabels(epochs)
+epochs = mne.read_epochs(fname.epochs(subject=subject))
+epochs.resample(256)
+epochs = epochs[["cond1", "cond2"]]
+epochs = epochs.crop(tmin=-0.1, tmax=1.0)
+labels = getEpochLabels(epochs)
 
-    selected_models = config["decoding_models"]
+selected_models = config["decoding_models"]
 
-    decoding_types = {
-        "LogisticRegression": 
-            sklearn.pipeline.Pipeline([
-                #("scaler", mne.decoding.Scaler(scalings='mean')),
-                ("transform", mne.decoding.Vectorizer()),
-                ("model", sklearn.linear_model.LogisticRegression(solver="lbfgs", max_iter=400)),
-                
-            ]),
-        "SVM":
-            sklearn.pipeline.Pipeline([
-                ("scaler", sklearn.preprocessing.StandardScaler()), #mne.decoding.Scaler(scalings='mean')
-                #("transform", mne.decoding.Vectorizer()),
-                ("model", sklearn.svm.LinearSVC(max_iter=400))
-            ])
-    }
+decoding_types = {
+    "LogisticRegression": 
+        sklearn.pipeline.Pipeline([
+            #("scaler", mne.decoding.Scaler(scalings='mean')),
+            ("transform", mne.decoding.Vectorizer()),
+            ("model", sklearn.linear_model.LogisticRegression(solver="lbfgs", max_iter=400)),
+            
+        ]),
+    "SVM":
+        sklearn.pipeline.Pipeline([
+            ("scaler", sklearn.preprocessing.StandardScaler()), #mne.decoding.Scaler(scalings='mean')
+            #("transform", mne.decoding.Vectorizer()),
+            ("model", sklearn.svm.LinearSVC(max_iter=400))
+        ])
+}
 
-    #Note
-    #Using this class is different from directly applying sklearn.preprocessing.StandardScaler
-    #  or sklearn.preprocessing.RobustScaler offered by scikit-learn. These scale each classification feature, 
-    # e.g. each time point for each channel, with mean and standard deviation computed across epochs, whereas mne.decoding.
-    # Scaler scales each channel using mean and standard deviation computed across all of its time points and epochs.
+#Note
+#Using this class is different from directly applying sklearn.preprocessing.StandardScaler
+#  or sklearn.preprocessing.RobustScaler offered by scikit-learn. These scale each classification feature, 
+# e.g. each time point for each channel, with mean and standard deviation computed across epochs, whereas mne.decoding.
+# Scaler scales each channel using mean and standard deviation computed across all of its time points and epochs.
 
+data = {}
+pipeline_list = [decoding_types[model] for model in selected_models]
 
-    # models = (("LogisticRegression", sklearn.linear_model.LogisticRegression(solver="lbfgs", max_iter=500)),
-    #             ("SVM", sklearn.svm.LinearSVC()),)
+for pipeline, model_type in zip(pipeline_list, selected_models):
+    #generate train/test split
+    cross_val = sklearn.model_selection.StratifiedShuffleSplit(10, test_size=0.3, random_state=7)
+    estimator = mne.decoding.SlidingEstimator(pipeline, scoring=config["decode_scoring"], n_jobs=n_jobs)
 
-    # feature_space = (("StandardScaler", sklearn.preprocessing.StandardScaler()),  # equals mne.decoding.Scaler(scalings='mean')
-    #                     ("Vectorizer", mne.decoding.Vectorizer()),
-    # )
-    fig, axs = plt.subplots(len(selected_models), 1, constrained_layout=True, figsize=(16, 10))
+    score = mne.decoding.cross_val_multiscore(estimator, epochs.get_data(), labels, cv=cross_val, n_jobs=n_jobs)
+    score = np.array(np.mean(score, axis=0))
+    time = epochs.times
+    t_max_score = time[np.argmax(score)]
 
-    fig.suptitle("Decoding Analysis")
-    data = {}
-    pipeline_list = [decoding_types[model] for model in selected_models]
+    data[model_type] = {"time": time, "score": score}
+    fig = plotData(time, score, model_type, t_max_score)
+    addFigure(subject, fig, "Decoding with model: {}".format(model_type), "Decoding")
 
-    for ax, pipeline, model_type in zip(axs.flatten(), pipeline_list, selected_models):
-        #generate train/test split
-        cross_val = sklearn.model_selection.StratifiedShuffleSplit(10, test_size=0.3, random_state=7)
-        estimator = mne.decoding.SlidingEstimator(pipeline, scoring=config["decode_scoring"], n_jobs=n_jobs)
-
-        score = mne.decoding.cross_val_multiscore(estimator, epochs.get_data(), labels, cv=cross_val, n_jobs=n_jobs)
-        score = np.array(np.mean(score, axis=0))
-        time = epochs.times
-        t_max_score = time[np.argmax(score)]
-
-        data[model_type] = {"time": time, "score": score}
-        plotData(ax, time, score, model_type, t_max_score)
-
-    addFigure(subject, fig, "Different decodings overview", "Decoding")
-
-    writeData(fname.decodingAnalysis(subject=subject), data)
-
-#data =
+writeData(fname.decodingAnalysis(subject=subject), data)
--- 10_decoding_02.py
+++ 10_decoding_02.py
@@ -10,88 +10,86 @@ import numpy as np
 import scipy
 import matplotlib.pyplot as plt
 from matplotlib import ticker
-import os
 
 # Decoding analysis Decode the main contrast of the experiment across time
 # RQ: When is information about the conditions in our data available?
 # https://mne.tools/stable/auto_tutorials/machine-learning/plot_sensors_decoding.html
-pvalues = []
-#cohensds = []
-score_mean = []
-time_lst = []
-time_lst_plt = []
+
+
+time_list = []
+time_plt = []
 
 subjects = handleSubjectArg(multiSub=True)
 
+time_list = np.arange(0, 1.0, config["decode_sampling"])
+score_time_array = np.array(np.zeros(shape=(len(subjects), len(time_list))))
 
-for start_time in np.arange(config["t_start"], config["t_end"], config["t_sample"]):
-    time_lst += [start_time]
-    time_lst_plt += [start_time, start_time + config["t_sample"]]
-    score_total = []
-    for subject in subjects:
-        subject_data = readData(fname.decodingAnalysis(subject=subject))
-        subject_data_model = subject_data[config["model_total_score"]]
-        time = subject_data_model["time"]
-        score = subject_data_model["score"]
 
-        time_idx = [i for i, x in enumerate(time) if x >= start_time and x < start_time + config["t_sample"]]
-        score = np.mean(score[time_idx[0]:time_idx[-1]+1])
-        #peaks.append([peak["peak_time"], peak["peak_alt"]])
-        score_total.append(score)
+for subject_idx, subject in enumerate(subjects):
+    #load subject data from step 01
+    subject_data = readData(fname.decodingAnalysis(subject=subject))
+    subject_data_model = subject_data[config["model_total_score"]]
+
+    subject_time = subject_data_model["time"]
+    subject_score = subject_data_model["score"]
+
     
-    score_mean += [np.mean(score_total)]
-    
+    for time_idx, time_point in enumerate(time_list):       
+        #find data in time bins
+        idx = [i for i, x in enumerate(subject_time) if x >= time_point  and x < time_point + config["decode_sampling"]]
+        #calculate mean score of bins
+        score = np.mean(subject_score[idx])
+        score_time_array[subject_idx, time_idx] = score
+        
+pvalues = []
+score_means = []
+time_means = []
 
+for time_idx, time_point in enumerate(time_list):
+    #calculate mean over time bins
+    subject_scores = score_time_array[:, time_idx]
+    score_means.append(np.mean(subject_scores))
+
+    #calculate p-values over time bins
     # Non-parametric cluster-level paired t-test
     # The first dimension should correspond to the difference between paired samples (observations) in two conditions.
     # https://mne.tools/dev/generated/mne.stats.permutation_cluster_1samp_test.html
+    t_stats, p_value = scipy.stats.ttest_1samp(subject_scores, 0.5, alternative='greater')
+    pvalues.append(p_value if p_value else nan)
+    if time_idx == len(time_list)-1:
+        time_means.append(time_point) #end at last timepoint
+    else:
+        time_means.append(np.mean([time_point, time_list[time_idx+1]]))
 
-    statistics, pvalue = scipy.stats.ttest_1samp(score_total, 0.5, alternative='greater')
-    pvalues.append(pvalue)
-    # Calculate effect size with Cohan's d
-    #cohensds.append((np.mean(score_total) - 0.5) / (np.sqrt((np.std(score_total) ** 2 + 0 ** 2) / 2)))
 
-#word = "not "*(p_value >= alpha)
-#print(f"Difference of ERP peak between face and car condition is {word}significant with alpha={alpha} and p-value={p_value}.")
-#print(test_results)
-pvalues = np.array(pvalues)
-fig, (ax1, ax2, ax3, ax4) = plt.subplots(4, 1, constrained_layout=True, gridspec_kw={'height_ratios': [4, 6, 1, 1]}, figsize=(14, 8))
-ax1.plot(time_lst_plt, np.repeat(score_mean, 2), label="score")
-ax1.axhline(y=0.5, color="lightcoral", linestyle='--', label="chance")  # Horizontal line indicating chance (50%)
-ax1.set_xlabel("Time [s]")
-ax1.set_ylabel("ROC AUC Score")
-ax1.set_xlim([0.0, 1.0])
-ax1.set_title(f"Average decoding score across time")
-ax1.legend()
-ax1.grid(linestyle="dashed")
-ax1.set_axisbelow(True)
-ax1.xaxis.set_major_locator(ticker.MultipleLocator(0.1))
+#plot data
+fig, ax = plt.subplots(2, 1, constrained_layout=True, gridspec_kw={'height_ratios': [5, 5]}, figsize=(16, 9))
 
-ax2.plot(time_lst_plt, np.repeat(pvalues, 2)*100, label="P-values")
-ax2.axhline(y=config["decode_alpha"]*100, linestyle='--', label="alpha={} %".format(config["decode_alpha"] * 100))  # Horizontal line indicating chance (50%)
-ax2.set_xlabel("Time [s]")
-ax2.set_ylabel("%")
-ax2.set_xlim([0.0, 1.0])
-ax2.set_ylim([0.0*100, 1.0*100])
-ax2.set_title(f"P-value of t-test across time")
-ax2.legend()
-ax2.grid(linestyle="dashed")
-ax2.set_axisbelow(True)
-ax2.xaxis.set_major_locator(ticker.MultipleLocator(0.1))
-ax2.yaxis.set_major_locator(ticker.MultipleLocator(0.05*100))
+#plot scores
+ax[0].plot(time_means, score_means, label="score")
+ax[0].axhline(y=0.5, color="red", linestyle='--', label="random")  # Horizontal line indicating chance (50%)
+ax[0].set_xlabel("Time [s]")
+ax[0].set_ylabel("Score: "+config["decode_scoring"])
+ax[0].set_xlim([0, 1])
+ax[0].set_title(f"Mean decoding score over subjects")
+ax[0].legend()
+ax[0].grid(alpha=0.25)
+ax[0].set_axisbelow(True)
+ax[0].xaxis.set_major_locator(ticker.MultipleLocator(0.1))
 
-ax3.pcolormesh(time_lst, [0,1], [pvalues < config["decode_alpha"]], cmap="RdYlGn")
-ax3.set_xlabel("Time [s]")
-ax3.set_xlim([0.0, 1.0])
-ax3.yaxis.set_visible(False)
-ax3.set_title(f"Significance of t-test across time")
-ax3.xaxis.set_major_locator(ticker.MultipleLocator(0.1))
 
-# ax4.pcolormesh(time_lst, [0,1], [np.clip(cohensds, a_min=0, a_max=3)], cmap="RdYlGn")
-# ax4.set_xlabel("Time [s]")
-# ax4.set_xlim([0.0, 1.0])
-# ax4.yaxis.set_visible(False)
-# ax4.set_title(f"Qualitative overview of effect size (Cohen's d) across time, d_max={np.max(cohensds):.3}, min_clipped at 0")
-# ax4.xaxis.set_major_locator(ticker.MultipleLocator(0.1))
+#plot p-values
+ax[1].plot(time_means, np.array(pvalues), label="P-values")
+ax[1].axhline(y=config["decode_alpha"], linestyle='--', color="red",label="alpha")
+ax[1].set_xlim([0, 1])
+ax[1].set_ylim([0, 1])
+ax[1].set_title(f"P-value over mean decoding score")
+ax[1].set_xlabel("Time [s]")
+ax[1].set_ylabel("p-values")
+ax[1].legend()
+ax[1].grid(alpha=0.25)
+ax[1].set_axisbelow(True)
+ax[1].xaxis.set_major_locator(ticker.MultipleLocator(0.05))
+ax[1].yaxis.set_major_locator(ticker.MultipleLocator(0.1))
 
 addFigure(None, fig, "Decoding Analysis overview", "Decoding", totalReport=True)
